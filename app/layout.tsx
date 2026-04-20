63cbc9a317ad74449e8056a133baeee5fc772556
AlenaLarzhevska14 <alenna11358@gmail.com>
2026-04-20 22:47:11 +0300

    add modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: ReactNode;
+  modal: ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -37,6 +39,7 @@ export default function RootLayout({
           <AuthProvider>
             <Header />
             {children}
+            {modal}
             <Footer />
           </AuthProvider>
         </TanStackProvider>
